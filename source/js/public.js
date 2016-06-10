4601aa7f4663c46aaa677df397dc0638faaedfb3
Zhang-leijie <3120100246@zju.edu.cn>
2016-06-10 23:29:17 +0800

    2016/06/10
    新增了关于我们和搜索界面，还未完成分页的添加

--- source/js/public.js
+++ source/js/public.js
@@ -1,13 +1,30 @@
-function swiper(){
-	var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    autoplay: 5000,
-    // 如果需要分页器
-    pagination: '.swiper-pagination',
-    paginationType : 'bullets',
-    // 如果需要前进后退按钮
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    
-  })
-}
+
+    function swiper(){
+    	var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        autoplay: 5000,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        paginationType : 'bullets',
+        // 如果需要前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        
+      })
+    }
+$(function(){
+    $(".searchicon").click(function() {
+        window.location.href="search.html";
+    });
+
+    $(".foottitle").click(function(event) {
+    $(this).next(".list").toggle(400);
+    $(this).children('.xiala').toggle(400);
+    $(this).children('.shangla').toggle(400);
+    });
+
+    $(".select").click(function(event) {
+    $(".head_list").toggle();
+    $(".select").toggleClass('show');
+    });
+});
